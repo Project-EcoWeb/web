84cac07557420c331eea5cd81859ca27c211c3dc
Ruan <oliveiraruan2018@gmail.com>
2025-09-17 15:39:53 -0400

    feat: add imports heroSection

--- ecoweb-site/app/page.tsx
+++ ecoweb-site/app/page.tsx
@@ -1,8 +1,11 @@
-import Image from "next/image";
 import { Header } from "@/components/header";
+import { HeroSection } from "@/components/hero-section";
 
 export default function Home() {
   return (
-    <Header />
+    <div className='min-h-screen'>
+      <Header />
+      <HeroSection />
+    </div>
   );
 }
