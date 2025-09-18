af942366de00a2462df17cc80a686a6d278ac94c
Ruan <oliveiraruan2018@gmail.com>
2025-09-18 15:17:27 -0400

    feat: add imports of components

--- ecoweb-site/app/page.tsx
+++ ecoweb-site/app/page.tsx
@@ -1,11 +1,16 @@
 import { Header } from "@/components/header";
 import { HeroSection } from "@/components/hero-section";
-
+import { ImpactStats } from "@/components/impact-stats";
+import { SolutionsSection } from "@/components/soluctions-section";
+import { Footer } from "@/components/footer";
 export default function Home() {
   return (
     <div className='min-h-screen'>
       <Header />
       <HeroSection />
+      <ImpactStats />
+      <SolutionsSection />
+      <Footer />
     </div>
   );
 }
